361199189d8026511c3aad80e0748f606fe255b4
Doug <doug@dougflynn.dev>
2023-08-22 12:21:00 -0700

    Add an error catcher for audd.io song matches

--- utils/audd.py
+++ utils/audd.py
@@ -3,13 +3,17 @@ import os
 
 
 async def audd_recognize_song(filename: str):
-    data = {
-        'api_token': os.getenv("AUDD_KEY"),
-        'return': 'apple_music,spotify',
-    }
-    files = {
-        'file': open(filename, 'rb'),
-    }
+    try:
+        data = {
+            'api_token': os.getenv("AUDD_KEY"),
+            'return': 'apple_music,spotify',
+        }
+        files = {
+            'file': open(filename, 'rb'),
+        }
 
-    result = requests.post('https://api.audd.io/', data=data, files=files)
-    return result.json()
+        result = requests.post('https://api.audd.io/', data=data, files=files)
+        print(result.content)
+        return result.json()
+    except requests.exceptions.JSONDecodeError:
+        return {"error": "Could not get audd.io source"}
