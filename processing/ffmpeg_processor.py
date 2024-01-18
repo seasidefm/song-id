d9c2834de6b8dd81c0d056bfda2a7a6e270ff1f8
Doug <doug@dougflynn.dev>
2024-01-17 16:14:00 -0800

    Add some doc strings to make things look prettier

--- processing/ffmpeg_processor.py
+++ processing/ffmpeg_processor.py
@@ -1,3 +1,5 @@
+import logging
+
 from ffmpeg.asyncio import ffmpeg
 
 from processing.VideoProcessor import VideoProcessor
@@ -5,6 +7,7 @@ from processing.VideoProcessor import VideoProcessor
 
 class FfmpegProcessor(VideoProcessor):
     async def process(self, input_file: str, output_file: str) -> str:
+        logging.info("FFMPEG: %s -> %s", input_file, output_file)
         await ffmpeg.FFmpeg().input(input_file).output(output_file).execute()
 
         return output_file
